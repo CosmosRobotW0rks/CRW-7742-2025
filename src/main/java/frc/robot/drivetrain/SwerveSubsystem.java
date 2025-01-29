dd6e657d6a95a97a5f5041a28f941505ce52508b
Bora <89869983+bora-sy@users.noreply.github.com>
2025-01-29 15:57:57 +0300

    Update PathPlanner

--- src/main/java/frc/robot/drivetrain/SwerveSubsystem.java
+++ src/main/java/frc/robot/drivetrain/SwerveSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.drivetrain;
 import static edu.wpi.first.units.Units.Inches;
 
 import java.io.Serial;
+import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +13,18 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 
 import com.ctre.phoenix6.controls.MotionMagicExpoDutyCycle;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.fasterxml.jackson.databind.node.TreeTraversingParser;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.controllers.PPLTVController;
+import com.pathplanner.lib.controllers.PathFollowingController;
 import com.studica.frc.AHRS;
 import com.studica.frc.AHRS.NavXComType;
 
+import edu.wpi.first.hal.AllianceStationID;
+import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -30,10 +40,12 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -44,23 +56,27 @@ import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.SwerveConstants;
+import frc.robot.util.Elastic;
+import frc.robot.util.Elastic.Notification.NotificationLevel;
 
 // NWU COORDINATES!!!!
 public class SwerveSubsystem extends SubsystemBase {
 
     XboxController joy;
-    
-    StructArrayPublisher<SwerveModuleState> swerveStatePublisher = NetworkTableInstance.getDefault().getStructArrayTopic("SwerveStates", SwerveModuleState.struct).publish();
-    StructPublisher<Pose2d> posePublisher = NetworkTableInstance.getDefault().getStructTopic("ChassisPose", Pose2d.struct).publish();
+
+    StructArrayPublisher<SwerveModuleState> swerveStatePublisher = NetworkTableInstance.getDefault()
+            .getStructArrayTopic("SwerveStates", SwerveModuleState.struct).publish();
+    StructPublisher<Pose2d> posePublisher = NetworkTableInstance.getDefault()
+            .getStructTopic("ChassisPose", Pose2d.struct).publish();
     Field2d odomDisplay = new Field2d();
 
     public AHRS gyro = new AHRS(NavXComType.kMXP_SPI);
 
     SwerveModule[] modules = new SwerveModule[] {
-        new SwerveModule(Constants.SwerveConstants.AngleCANID_FL, Constants.SwerveConstants.DriveCANID_FL),
-        new SwerveModule(Constants.SwerveConstants.AngleCANID_FR, Constants.SwerveConstants.DriveCANID_FR),
-        new SwerveModule(Constants.SwerveConstants.AngleCANID_BL, Constants.SwerveConstants.DriveCANID_BL),
-        new SwerveModule(Constants.SwerveConstants.AngleCANID_BR, Constants.SwerveConstants.DriveCANID_BR)
+            new SwerveModule(Constants.SwerveConstants.AngleCANID_FL, Constants.SwerveConstants.DriveCANID_FL),
+            new SwerveModule(Constants.SwerveConstants.AngleCANID_FR, Constants.SwerveConstants.DriveCANID_FR),
+            new SwerveModule(Constants.SwerveConstants.AngleCANID_BL, Constants.SwerveConstants.DriveCANID_BL),
+            new SwerveModule(Constants.SwerveConstants.AngleCANID_BR, Constants.SwerveConstants.DriveCANID_BR)
     };
 
     SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
@@ -71,42 +87,59 @@ public class SwerveSubsystem extends SubsystemBase {
 
     ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0, 0, 0);
 
-
     final Pose2d initialPose = new Pose2d(0, 0, new Rotation2d(0));
 
-    SwerveDrivePoseEstimator poseEstimator = new SwerveDrivePoseEstimator(kinematics, GetRobotHeading(), GetModulePositions(), initialPose);
+    SwerveDrivePoseEstimator poseEstimator = new SwerveDrivePoseEstimator(kinematics, GetRobotHeading(),
+            GetModulePositions(), initialPose);
 
     Vision vision = new Vision("AACAM");
 
-    public SwerveSubsystem(XboxController joy)
-    {
+    public SwerveSubsystem(XboxController joy) {
         this.joy = joy;
-
         SmartDashboard.putData("Field", odomDisplay);
 
-        SmartDashboard.putData("HOME", Commands.runOnce(() -> {
-            for (SwerveModule module : modules) {
-                Rotation2d currentAngle = module.GetAngle();
+        InitializePathPlanner();
 
-                Rotation2d targetAngle = Rotation2d.fromRadians(currentAngle.getRadians() % (2*Math.PI));
-
-                module.SetTargetAngle(targetAngle);
-                System.out.printf("Module Angle: %f", targetAngle.getDegrees());
-            }
-
-        }));
     }
 
-    
-    void ApplyChassisSpeeds(ChassisSpeeds speeds)
-    {
+    void InitializePathPlanner() {
 
-        SmartDashboard.putNumber("AAAAAA", 1000);
+        try {
+
+            RobotConfig config = RobotConfig.fromGUISettings();
+
+            AutoBuilder.configure(
+                    () -> GetRobotPose(),
+                    p2d -> {
+                    },
+                    () -> GetChassisSpeeds(),
+                    (ChassisSpeeds, DriveFeedforwards) -> {
+                    },
+                    new PPHolonomicDriveController(
+                            // PPHolonomicController is the built in path following controller for holonomic
+                            // drive trains
+                            new PIDConstants(5.0, 0.0, 0.0),
+                            // Translation PID constants
+                            new PIDConstants(5.0, 0.0, 0.0)
+                    // Rotation PID constants
+                    ),
+                    config,
+                    () -> {
+                        Optional<Alliance> alliance = DriverStation.getAlliance();
+                        return alliance.isPresent() && alliance.get() == Alliance.Red;
+                    }, this);
+
+        } catch (Exception ex) {
+            Elastic.sendNotification(new Elastic.Notification(NotificationLevel.ERROR,
+                    "CRITICAL ERROR! PATHPLANNER FAILED TO INITIALIZE", ex.getMessage()));
+        }
+    }
+
+    void ApplyChassisSpeeds(ChassisSpeeds speeds) {
 
         SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
 
-        for (int i = 0; i < moduleStates.length; i++)
-        {
+        for (int i = 0; i < moduleStates.length; i++) {
             SwerveModuleState state = moduleStates[i];
             SwerveModule module = modules[i];
 
@@ -116,10 +149,9 @@ public class SwerveSubsystem extends SubsystemBase {
         }
     }
 
-    void optimizeModuleState(SwerveModuleState state, Rotation2d currentAngle)
-    {
+    void optimizeModuleState(SwerveModuleState state, Rotation2d currentAngle) {
         double currentRad = currentAngle.getRadians();
-        double targetRad = state.angle.getRadians() % (Math.PI*2);
+        double targetRad = state.angle.getRadians() % (Math.PI * 2);
 
         double min = Math.floor(currentRad / targetRad) * targetRad;
         double max = min + targetRad;
@@ -127,82 +159,71 @@ public class SwerveSubsystem extends SubsystemBase {
         double minDiff = Math.abs(currentRad - min);
         double maxDiff = Math.abs(currentRad - max);
 
-        if(minDiff < maxDiff)
-        {
+        if (minDiff < maxDiff) {
             state.angle = Rotation2d.fromRadians(min);
-        }
-        else state.angle = Rotation2d.fromRadians(max);
+        } else
+            state.angle = Rotation2d.fromRadians(max);
     }
 
-    void UpdatePoseEstimation()
-    {
+    void UpdatePoseEstimation() {
         // TODO: Tune deviations
-        
+
         Pose2d robotPose = poseEstimator.update(GetRobotHeading(), GetModulePositions());
         odomDisplay.setRobotPose(robotPose);
     }
 
-    void ResetOdometryWithVision()
-    {
+    void ResetOdometryWithVision() {
         Optional<EstimatedRobotPose> optPose = vision.GetEstimatedVisionPose();
 
-        if(optPose == null || !optPose.isPresent()) return;
+        if (optPose == null || !optPose.isPresent())
+            return;
 
         EstimatedRobotPose camPose = optPose.get();
-        
+
         poseEstimator.addVisionMeasurement(camPose.estimatedPose.toPose2d(), camPose.timestampSeconds);
     }
 
-    public void SetChassisSpeeds(ChassisSpeeds speeds)
-    {
+    public void SetChassisSpeeds(ChassisSpeeds speeds) {
         chassisSpeeds = speeds;
 
-        SmartDashboard.putNumberArray("ChassisSpeeds", new Double[] {speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond});
+        SmartDashboard.putNumberArray("ChassisSpeeds",
+                new Double[] { speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond });
     }
 
-    public ChassisSpeeds GetChassisSpeeds()
-    {
+    public ChassisSpeeds GetChassisSpeeds() {
         return chassisSpeeds;
     }
 
-
-    public void SetFieldOrientedChassisSpeeds(double xSpeed, double ySpeed, double rotSpeed)
-    {
+    public void SetFieldOrientedChassisSpeeds(double xSpeed, double ySpeed, double rotSpeed) {
         Rotation2d heading = GetRobotHeading();
         ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rotSpeed, heading);
-        
+
         SetChassisSpeeds(speeds);
     }
 
     public Rotation2d GetRobotHeading() {
-        return Rotation2d.fromDegrees(360.0-gyro.getFusedHeading());
+        return Rotation2d.fromDegrees(360.0 - gyro.getFusedHeading());
     }
 
-    public SwerveModuleState[] GetModuleStates()
-    {
+    public SwerveModuleState[] GetModuleStates() {
         SwerveModuleState[] states = new SwerveModuleState[modules.length];
-        for (int i = 0; i < modules.length; i++)
-        {
+        for (int i = 0; i < modules.length; i++) {
             states[i] = modules[i].GetState();
         }
         return states;
     }
 
-    public SwerveModulePosition[] GetModulePositions()
-    {
+    public SwerveModulePosition[] GetModulePositions() {
         SwerveModulePosition[] positions = new SwerveModulePosition[modules.length];
-        for (int i = 0; i < modules.length; i++)
-        {
+        for (int i = 0; i < modules.length; i++) {
             positions[i] = modules[i].GetPosition();
         }
         return positions;
     }
 
-    public Pose2d GetRobotPose()
-    {
+    public Pose2d GetRobotPose() {
         return poseEstimator.getEstimatedPosition();
     }
-
 
     @Override
     public void periodic() {
